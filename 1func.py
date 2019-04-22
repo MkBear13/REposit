4ca31650ffcb6d860e889b601b2d5b611459e717
Mk_bear13 <manutdsinse1878@gmail.com>
2019-04-22 19:59:54 +0300

    With using files

--- 1func.py
+++ 1func.py
@@ -1,8 +1,16 @@
-list_of_elements = [1,2,3,4,4]
+from context_manager import ContextManager
+
+with ContextManager(r'C:\Users\Mikhail.Kavaliov\Desktop\New folder\for_first.txt', 'r') as open_file:
+          list_of_numbers_from_file = list(map(int, open_file.read().split(','))) 
+
+
 def povtor(list_of_elements):
     set_a = set(list_of_elements)
     max = 0
     for x in set_a:
         if list_of_elements.count(x) > max:
             max = list_of_elements.count(x)
-    return(max)
+    return max
+
+with ContextManager(r'C:\Users\Mikhail.Kavaliov\Desktop\New folder\for_first.txt', 'a') as open_file:
+    open_file.write('\nMax element is: ' + str(povtor(list_of_numbers_from_file)))
