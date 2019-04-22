d91ff25f33c1799abf6a525e7faf77008f8c430f
Mk_bear13 <manutdsinse1878@gmail.com>
2019-04-22 19:12:16 +0300

    Real sorting

--- 5func.py
+++ 5func.py
@@ -1,4 +1,12 @@
-def sortirovka(list1):
-    set_a=set(list1)
-    list2=list(set_a)
-    print(list2)
+def sorting(list_of_data):
+    for i in range(len(list_of_data) - 1):
+        for j in range(len(list_of_data) - i - 1):
+            if list_of_data[j] > list_of_data[j + 1]:
+                buff = list_of_data[j]
+                list_of_data[j] = list_of_data[j + 1]
+                list_of_data[j + 1] = buff
+    return list_of_data
+
+
+list1 = [1, 2, 3, 5, 9, 4, 17, 6, 13]
+print(sorting(list1))
