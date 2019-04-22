66d5442c0db7a4373e707dc3e881a0c4c065e6ca
Mk_bear13 <manutdsinse1878@gmail.com>
2019-04-22 19:20:05 +0300

    fixes problem

--- 4func.py
+++ 4func.py
@@ -6,6 +6,4 @@ def leap(year):
 
 
 def leap_2(year):
-    if year%400==0  or (year%4==0 and year%100!=0):
-        return (True)
-    return(False)      
+    return year%400 == 0  or (year%4 == 0 and year%100 != 0)
