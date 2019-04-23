7b391278865c3a52751881058d7773ca39a4316b
Mk_bear13 <manutdsinse1878@gmail.com>
2019-04-23 12:11:31 +0300

    Add work with file

--- 6func.py
+++ 6func.py
@@ -1,11 +1,15 @@
-import time
+from context_manager import ContextManager
+
+with ContextManager(r'C:\Users\Mikhail.Kavaliov\Desktop\New folder\for_sixth.txt', 'r') as open_file:
+          number_from_file = int(open_file.read()) 
 
 
-start_time = time.time()
 def element(n):
     znach = 0
     for i in range(1, n+1):
        znach += i**i
     wwznach = str(znach)
-    return(ad[-10:])
-print("--- %s seconds ---" % (time.time() - start_time))
+    return(wwznach[-10:])
+
+with ContextManager(r'C:\Users\Mikhail.Kavaliov\Desktop\New folder\for_sixth.txt', 'a') as open_file:
+    open_file.write('\nLast 10 symbols : ' + str(element(number_from_file)))
