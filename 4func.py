a25fe897f0318a3f3ba6c9611b8587548a3de7eb
Mk_bear13 <manutdsinse1878@gmail.com>
2019-04-23 11:59:50 +0300

    add working with files

--- 4func.py
+++ 4func.py
@@ -1,5 +1,10 @@
 import calendar
 
+from context_manager import ContextManager
+
+with ContextManager(r'C:\Users\Mikhail.Kavaliov\Desktop\New folder\for_fourth.txt', 'r') as open_file:
+          year_from_file = int(open_file.read()) 
+
 
 def leap(year):
     return (calendar.isleap(year))
@@ -7,3 +12,7 @@ def leap(year):
 
 def leap_2(year):
     return year%400 == 0  or (year%4 == 0 and year%100 != 0)
+
+with ContextManager(r'C:\Users\Mikhail.Kavaliov\Desktop\New folder\for_fourth.txt', 'a') as open_file:
+    open_file.write('\n' + str(leap(year_from_file)))
+    open_file.write('\n' + str(leap_2(year_from_file)))
